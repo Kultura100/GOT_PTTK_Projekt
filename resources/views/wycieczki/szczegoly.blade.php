df8d5a4da9f17f946a5e4cc9124713a6744cf603
TheDudusieX <lukaszd552d@gmail.com>
2022-04-12 01:31:30 +0200

    front

--- resources/views/wycieczki/szczegoly.blade.php
+++ resources/views/wycieczki/szczegoly.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
-    {{-- <x-slot name="styles">
-      <link rel="stylesheet" type="text/css" href="{{ asset('css/raport.css') }}">
+<x-slot name="styles">
+      <link rel="stylesheet" type="text/css" href="{{ asset('css/szczegoly_wycieczki.css') }}">
     </x-slot>
-    <x-slot name="scripts">
-      <script src="{{ asset('js/raport.js') }}"></script>
-    </x-slot> --}}
     <div class="container">
       <h1>Szczegóły wycieczki</h1>
+      <div class="d-flex flex-row-reverse mt-5">
+      <a href="{{ route('wycieczki.index') }}"
+      type="button"
+      class="btn btn-primary"
+      role="button">
+    {{ __('translations.wycieczki.label.back') }}
+    </a>
+      </div>
             <table class="table">
-              <thead>
+              <thead >
                   <tr>
                       <th scope='col'>Imie i nazwisko twórcy</th>                      
                       <th scope='col'>Punkt początkowy</th>
@@ -19,29 +24,27 @@
               </thead>   
                   <tbody>
                       <tr>     
-                          <th scope='row'>{{ $wycieczki->uzytkownikWycieczka->imie}} {{ $wycieczki->uzytkownikWycieczka->nazwisko}}</th>
-                          <th scope='row'>
-                            {{-- @foreach($wycieczki->wieleodcinkow as $odcinek)
-                              {{$odcinek->wycieczka_odcinek2->punktpocz->nazwa}}
-                            @endforeach --}}
+                          <td>{{ $wycieczki->uzytkownikWycieczka->imie}} {{ $wycieczki->uzytkownikWycieczka->nazwisko}}</td>
+                          <td>
                             {{ $wycieczki->wieleodcinkow->first()->wycieczka_odcinek2->punktpocz->nazwa}}
-                          </th>
-                          <th scope='row'>
+                          </td>
+                          <td>
                             @foreach($wycieczki->wieleodcinkow as $odcineczek)
                                     @if($odcineczek->wycieczka_odcinek2->otwarty == true)
                                         {{$odcineczek->wycieczka_odcinek2->punktpocz->nazwa }} - {{ $odcineczek->wycieczka_odcinek2->punktkoncz->nazwa}}, 
                                     @endif
                             @endforeach
-                        </th>
-                        <th scope='row'>
+                          </td>
+                        <td>
                           @foreach($wycieczki->wieleodcinkow as $odcineczek)
-                          @if($odcineczek->wycieczka_odcinek2->otwarty == false)
-                              {{$odcineczek->wycieczka_odcinek2->punktpocz->nazwa }} - {{ $odcineczek->wycieczka_odcinek2->punktkoncz->nazwa}}
-                          @endif
-                  @endforeach
-                        </th>                          
-                          <th scope='row'>{{  $wycieczki->wieleodcinkow->last()->wycieczka_odcinek2->punktkoncz->nazwa}}</th>
-                          </tbody>
-            </table>                              
-          </div>
+                            @if($odcineczek->wycieczka_odcinek2->otwarty == false)
+                                {{$odcineczek->wycieczka_odcinek2->punktpocz->nazwa }} - {{ $odcineczek->wycieczka_odcinek2->punktkoncz->nazwa}}
+                            @endif
+                          @endforeach
+                        </td>                          
+                          <td>{{  $wycieczki->wieleodcinkow->last()->wycieczka_odcinek2->punktkoncz->nazwa}}</td>
+                      </tr>
+                  </tbody>
+          </table>                              
+      </div>
 </x-app-layout>
