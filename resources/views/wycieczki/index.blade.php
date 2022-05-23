8dd4a75339c684132a6feefeb917c16516c34c39
Owca1111 <owca.majki25.pl@gmail.com>
2022-05-23 22:26:16 +0200

    Toasty_aktualizacja

--- resources/views/wycieczki/index.blade.php
+++ resources/views/wycieczki/index.blade.php
@@ -31,26 +31,30 @@
 
     <ul class="responsive-table">
         <li class="table-header">
-            <div class="col col-1">Data początku</div>
-            <div class="col col-2">Data końca</div>
-            <div class="col col-3">Pasmo</div>
-            <div class="col col-4">Ilość punktów</div>
-            <div class="col col-6">Operacje</div>
+            <div class="col col-1"><strong>Nazwa</strong></div>
+            <div class="col col-2"><strong>Data początku</strong></div>
+            <div class="col col-3"><strong>Data końca</strong></div>
+            <div class="col col-4"><strong>Pasmo</strong></div>
+            <div class="col col-5"><strong>Ilość punktów</strong></div>
+            <div class="col col-6"><strong>Operacje</strong></div>
         </li>
       
         @foreach ($wycieczki as $wycieczka)
         {{-- @dd($wycieczka->wieleodcinkow->first()->wycieczka_odcinek2->pasmo->nazwa) --}}
             <li class="table-row">
                 <div class="col col-1">
-                    <!-- data poczatku wycieczki -->{{ $wycieczka->dataod }}
+                    <!-- Nazwa wycieczki -->{{ $wycieczka->nazwa }}
                 </div>
                 <div class="col col-2">
-                    <!-- data konca wycieczki -->{{ $wycieczka->datado }}
+                    <!-- data poczatku wycieczki -->{{ $wycieczka->dataod }}
                 </div>
                 <div class="col col-3">
-                    <!-- nazwa pasma -->@if(null !== $wycieczka->wieleodcinkow->first()) {{$wycieczka->wieleodcinkow->first()->wycieczka_odcinek2->pasmo->nazwa }}@endif
+                    <!-- data konca wycieczki -->{{ $wycieczka->datado }}
                 </div>
                 <div class="col col-4">
+                    <!-- nazwa pasma -->@if(null !== $wycieczka->wieleodcinkow->first()) {{$wycieczka->wieleodcinkow->first()->wycieczka_odcinek2->pasmo->nazwa }}@endif
+                </div>
+                <div class="col col-5">
                     <!-- ilosc zebranych punktow -->{{ $wycieczka->punkty }}
                 </div>
 
@@ -81,7 +85,7 @@
         <li class="table-header">
             <div class="col"></div>
             <div class="col"></div>
-            <div class="col justify-center align-middle">Odbyte</div>
+            <div class="col justify-center align-middle"><strong>Odbyte</strong></div>
             <div class="col"></div>
             <div class="col"></div>
         </li>
@@ -89,16 +93,19 @@
         @foreach ($wycieczkiodbyte as $wycieczka)
         {{-- @dd($wycieczka->wieleodcinkow->first()->wycieczka_odcinek2->pasmo->nazwa) --}}
             <li class="table-row">
-                <div class="col col-1">
-                    <!-- data poczatku wycieczki -->{{ $wycieczka->dataod }}
+             <div class="col col-1">
+                    <!-- Nazwa wycieczki -->{{ $wycieczka->nazwa }}
                 </div>
                 <div class="col col-2">
-                    <!-- data konca wycieczki -->{{ $wycieczka->datado }}
+                    <!-- data poczatku wycieczki -->{{ $wycieczka->dataod }}
                 </div>
                 <div class="col col-3">
-                    <!-- nazwa pasma -->@if(null !== $wycieczka->wieleodcinkow->first()) {{$wycieczka->wieleodcinkow->first()->wycieczka_odcinek2->pasmo->nazwa }}@endif
+                    <!-- data konca wycieczki -->{{ $wycieczka->datado }}
                 </div>
                 <div class="col col-4">
+                    <!-- nazwa pasma -->@if(null !== $wycieczka->wieleodcinkow->first()) {{$wycieczka->wieleodcinkow->first()->wycieczka_odcinek2->pasmo->nazwa }}@endif
+                </div>
+                <div class="col col-5">
                     <!-- ilosc zebranych punktow -->{{ $wycieczka->punkty }}
                 </div>
 
